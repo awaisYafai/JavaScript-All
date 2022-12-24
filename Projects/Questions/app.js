b470ff330925c54579918c08fbd299615303b68b
Awais Yafai <awaisyafai0079@gmail.com>
2022-12-24 14:26:35 +0530

    project

--- Projects/Questions/app.js
+++ Projects/Questions/app.js
@@ -1,22 +1,22 @@
-//using selectors inside the element
-const questions = document.querySelectorAll(".question");
+// //using selectors inside the element
+// const questions = document.querySelectorAll(".question");
 
-questions.forEach(function (question) {
-  const btn = question.querySelector(".question-btn");
-  // console.log(btn);
+// questions.forEach(function (question) {
+//   const btn = question.querySelector(".question-btn");
+//   // console.log(btn);
 
-  btn.addEventListener("click", function () {
-    // console.log(question);
+//   btn.addEventListener("click", function () {
+//     // console.log(question);
 
-    questions.forEach(function (item) {
-      if (item !== question) {
-        item.classList.remove("show-text");
-      }
-    });
+//     questions.forEach(function (item) {
+//       if (item !== question) {
+//         item.classList.remove("show-text");
+//       }
+//     });
 
-    question.classList.toggle("show-text");
-  });
-});
+//     question.classList.toggle("show-text");
+//   });
+// });
 
 // traversing the dom
 // const btns = document.querySelectorAll(".question-btn");
@@ -26,5 +26,34 @@ questions.forEach(function (question) {
 //     const question = e.currentTarget.parentElement.parentElement;
 
 //     question.classList.toggle("show-text");
+//   });
+// });
+
+// using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', function(){
+    questions.forEach(function(item){
+      if (item !== question){
+        item.classList.remove('show-text');
+      }
+    })
+    question.classList.toggle('show-text');
+
+
+  });
+});
+
+
+// traversing the dom 
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(e){
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
 //   });
 // });
