8d8215145803f89448133c228e950f0290eb7776
Awais Yafai <awaisyafai0079@gmail.com>
2023-01-12 20:15:26 +0530

    suneja

--- JS-Suneja/callBackPromiseAsyncAwaits.js
+++ JS-Suneja/callBackPromiseAsyncAwaits.js
@@ -1,6 +1,7 @@
 // async in programming
 // callbacks, Promises, async, await
 
+// call back
 // const datas = [
 //   { name: "awais", profession: "developer" },
 //   { name: "akhib", profession: "developer" },
@@ -23,6 +24,37 @@
 // creatData({ name: "asif", profession: "developer" }, getDatas);
 // console.log(datas);
 
+// Promise
+// const datas = [
+//   { name: "awais", profession: "developer" },
+//   { name: "akhib", profession: "developer" },
+// ];
+
+// function getDatas() {
+//   setTimeout(() => {
+//     datas.forEach((data, index) => {
+//       console.log(data.name);
+//     });
+//   }, 1000);
+// }
+
+// function creatData(newData) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       datas.push(newData);
+//       let error = true;
+//       if (!error) {
+//         resolve();
+//       } else {
+//         reject("not well..");
+//       }
+//     }, 2000);
+//   });
+// }
+// creatData({ name: "asif", profession: "developer" }).then(getDatas).catch(err => console.log(err));
+// console.log(datas);
+
+// async & await
 const datas = [
   { name: "awais", profession: "developer" },
   { name: "akhib", profession: "developer" },
@@ -40,8 +72,8 @@ function creatData(newData) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       datas.push(newData);
-      let error = false;
-      if (error) {
+      let error = true;
+      if (!error) {
         resolve();
       } else {
         reject("not well..");
@@ -49,5 +81,11 @@ function creatData(newData) {
     }, 2000);
   });
 }
-creatData({ name: "asif", profession: "developer" }).then(getDatas).catch(err => console.log(err));
-console.log(datas);
+
+async function start() {
+  await creatData({ name: "asif", profession: "developer" })
+    .then(getDatas)
+    .catch((err) => console.log(err));
+  getDatas();
+}
+start();
