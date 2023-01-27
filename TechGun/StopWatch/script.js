bffbd6492c4ac6fcedc1dac9a8e5f38e5a291f00
Awais Yafai <awaisyafai0079@gmail.com>
2023-01-28 02:24:00 +0530

    project

--- TechGun/StopWatch/script.js
+++ TechGun/StopWatch/script.js
@@ -14,7 +14,17 @@ function stop() {
   timer = false;
 }
 
-function reset() {}
+function reset() {
+  timer = false;
+  hour = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
+  document.getElementById("hour").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
+}
 
 function stopWatch() {
   if (timer == true) {
@@ -29,14 +39,33 @@ function stopWatch() {
       sec = 0;
     }
     if (min == 60) {
-      hour= hour + 1;
+      hour = hour + 1;
       min = 0;
       sec = 0;
     }
-    document.getElementById("hour").innerHTML = hour;
-    document.getElementById("min").innerHTML = min;
-    document.getElementById("sec").innerHTML = sec;
-    document.getElementById("count").innerHTML = count;
+
+    let hourString = hour;
+    let minString = min;
+    let secString = sec;
+    let countString = count;
+
+    if (hour < 10) {
+      hourString = "0" + hourString;
+    }
+    if (min < 10) {
+      minString = "0" + minString;
+    }
+    if (sec < 10) {
+      secString = "0" + secString;
+    }
+    if (count < 10) {
+      countString = "0" + countString;
+    }
+
+    document.getElementById("hour").innerHTML = hourString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
+    document.getElementById("count").innerHTML = countString;
     setTimeout("stopWatch()", 10);
   }
 }
